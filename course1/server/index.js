d559a1ae3fa16af55fc3a7726fd22593db933420
carpinetti <carpinetti28@hotmail.com>
2018-01-31 22:47:01 -0200

    Update up to lecture 20

--- course1/server/index.js
+++ course1/server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-})
+// clientID: 980600566381-a62ar9abe4lim2pvhmlo8dcdt21cphbd.apps.googleusercontent.com
+// clientSecred: v_tEc7MhbP182kzsD19gZB5F
+passport.use(new GoogleStategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
