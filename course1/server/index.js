0b378d9ceb1bb85595c23cf77cd29b07fa9d4f94
carpinetti <carpinetti28@hotmail.com>
2018-02-09 23:18:14 -0200

    Modulo 4, aula 35

--- course1/server/index.js
+++ course1/server/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStategy = require('passport-google-oauth20').Strategy;
+require('./services/passport');
 
 const app = express();
 
-// clientID: 980600566381-a62ar9abe4lim2pvhmlo8dcdt21cphbd.apps.googleusercontent.com
-// clientSecred: v_tEc7MhbP182kzsD19gZB5F
-passport.use(new GoogleStategy());
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
