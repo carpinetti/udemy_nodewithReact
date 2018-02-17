decb5386e7e732892937435c4d85cacd718beb5b
carpinetti <carpinetti28@hotmail.com>
2018-02-16 23:43:15 -0200

    including done for passaport.

--- course1/server/services/passport.js
+++ course1/server/services/passport.js
@@ -3,7 +3,7 @@ const GoogleStategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys.js');
 
-const User = mongoose.model('users');
+const Users = mongoose.model('users');
 
 passport.use(
     new GoogleStategy(
@@ -13,7 +13,18 @@ passport.use(
             callbackURL: '/auth/google/callback',
         },
         (accessToken, refreshToken, profile, done) => {
-            new User( { googleId: profile.id }).save();
+            Users.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if(existingUser) {
+                    // we alredy have a record with the given profile ID
+                    done(null, existingUser);
+                } else  {
+                    // we don't have a user record with this ID, make a new record.
+                    new Users( { googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+                }
+            })
         }
     )
 );
